82fe402c0f812fe8a094488d724d1cc0819c5379
FauruxD <faruuxd@gmail.com>
2026-02-12 19:32:52 +0700

    Log completion message after vending action

--- MagToVend.lua
+++ MagToVend.lua
@@ -8,3 +8,4 @@ Sleep(200)
 SendPacket(2,"action|dialog_return\ndialog_name|vending\ntilex|"..x.."|\ntiley|"..y.."|\nbuttonClicked|addstocks\n\nsetprice|0\nchk_peritem|1\nchk_perlock|0\n")
 Sleep(200)
 end
+log("`2DONE")
