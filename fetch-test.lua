aa7c429f04282642af2bb004cbc7d9e8cb6be88c
FauruxD <faruuxd@gmail.com>
2026-02-13 11:07:38 +0700

    Update fetch-test.lua

--- fetch-test.lua
+++ fetch-test.lua
@@ -1,19 +1,33 @@
 run = false
 
-function start()
-    while run do
-      sendPacket(2,"action|input\n|text|Hai Nama Saya "..getLocal().name)
-      sleep(2000)
-    end
+function startLoop()
+    runThread(function()
+        while run do
+            sendPacket(2, "action|input\n|text|Hai Nama Saya "..getLocal().name)
+            sleep(2000)
+        end
+    end)
 end
 
-AddHook(function(type, pkt)
-  if pkt:find("/start") then
-    run = true
-  elseif pkt:find("/stop") then
-    run = false
-      return true
-  end
-"OnSendPacket", end)
+AddHook("OnSendPacket", function(type, pkt)
 
-  SendVariantList({v1 = 'OnTalkBubble',v2 = GetLocal().netID,v3 = "`9Script Fetched! Enjoy",v4 = 0,v5 = 0}, -1, 1000)
+    if pkt:find("/start") then
+        if not run then
+            run = true
+            startLoop()
+        end
+        return true
+    end
+
+    if pkt:find("/stop") then
+        run = false
+        return true
+    end
+
+end)
+
+SendVariantList(
+    {v1 = "OnTalkBubble", v2 = GetLocal().netID, v3 = "`9Script Fetched! Enjoy", v4 = 0, v5 = 0},
+    -1,
+    1000
+)
