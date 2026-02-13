aa906f34f383cc383aed3a3cd81185a734150eb7
FauruxD <faruuxd@gmail.com>
2026-02-13 11:10:38 +0700

    Fix typo in event name from 'onSendPacket' to 'OnSendPacket'

--- fetch-test.lua
+++ fetch-test.lua
@@ -24,7 +24,7 @@ AddHook(function(type, packet)
         return true
     end
 
-"onSendPacket", end)
+"OnSendPacket", end)
 
 SendVariantList(
     {v1 = "OnTalkBubble", v2 = GetLocal().netID, v3 = "`9Script Fetched! Enjoy", v4 = 0, v5 = 0},
