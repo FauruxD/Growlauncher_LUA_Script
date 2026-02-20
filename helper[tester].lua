773429259ca27854ff88a5358bbc68f7fa497bcb
FauruxD <faruuxd@gmail.com>
2026-02-20 20:33:25 +0700

    Update helper[tester].lua

--- helper[tester].lua
+++ helper[tester].lua
@@ -11,6 +11,7 @@ seedId = 333
 plantId = 5640
 delayPlant = 25
 delayHarvest = 80
+platformId = 7520
 countPtht = 1
 magx = 27
 magy = 109
@@ -175,7 +176,7 @@ end
 
 function bellow(x, y)
     local below = GetTile(x, y +1)
-    return below and below.fg == 7520
+    return below and below.fg == platformId
 end
 
 function plant()
@@ -410,6 +411,7 @@ text_scaling_string|helperui|
 add_button_with_icon|togglepnb|`7PNB Helper|staticGreyFrame|9472||
 add_button_with_icon|featurescript|`7Feature Script|staticGreyFrame|9472||
 add_button_with_icon|configpnb|`7PNB Config|staticGreyFrame|32||
+add_button_with_icon|configPtht|`7PTHT Config|staticGreyFrame|32||
 add_button_with_icon|exchange_go|`7Exchange Menu|staticGreyFrame|12826||
 add_button_with_icon||END_LIST|noflags|0||
 add_spacer|small|
@@ -550,6 +552,7 @@ add_label_with_icon|small|`7Mode : ptht|left|14922|
 add_label_with_icon|small|`7Spray Mode : uws|left|5926|
 add_label_with_icon|small|`7ID Plant : 5640 - `9]]..growtopia.getItemName(5640)..[[|left|5640|
 add_label_with_icon|small|`7ID Harvest : 333 - `9]]..growtopia.getItemName(333)..[[|left|333|
+add_label_with_icon|small|`7ID Harvest : 7520 - `9]]..growtopia.getItemName(7520)..[[|left|7520|
 add_label_with_icon|small|`7Delay Plant : 25|left|3804|
 add_label_with_icon|small|`7Delay Harvest : 80|left|3804|
 add_label_with_icon|small|`7Pos Mag : X: 27 Y: 109|left|5638|
@@ -592,6 +595,7 @@ add_label_with_icon|small|`7Mode : ]]..mode..[[|left|14922|
 add_label_with_icon|small|`7Spray Mode : ]]..sprayMode..[[|left|5926|
 add_label_with_icon|small|`7ID Plant : ]]..plantId..[[ - `9]]..growtopia.getItemName(plantId)..[[|left|]]..plantId..[[|
 add_label_with_icon|small|`7ID Harvest : ]]..seedId..[[ - `9]]..growtopia.getItemName(seedId)..[[|left|]]..seedId..[[|
+add_label_with_icon|small|`7ID Harvest : ]]..platformId..[[ - `9]]..growtopia.getItemName(platformId)..[[|left|]]..platformId..[[|
 add_label_with_icon|small|`7Delay Plant : ]]..delayPlant..[[|left|3804|
 add_label_with_icon|small|`7Delay Harvest : ]]..delayHarvest..[[|left|3804|
 add_label_with_icon|small|`7Pos Mag : X: ]]..magx..[[ Y: ]]..magy..[[|left|5638|
@@ -722,6 +726,11 @@ function hook(type, str)
         return true
     end
 
+	if str:find("buttonClicked|configPtht") then
+        configPtht()
+        return true
+    end
+
     if str:find("/infoo") then
         infoPtht()
         return true
@@ -745,6 +754,10 @@ function hook(type, str)
 	end
 
     -- exchange commands
+	if str:find("buttonClicked|exchange_go") then
+        growtopia.sendChat("/exchange", true)
+        return true
+    end
     if str:find("/startexc") then
         startExchangeEngine()
         talkBubble("`2Exchange Mode Started! go /exchange and press `9GET")
@@ -966,4 +979,5 @@ talkBubble("`9Script Loaded! `9Type `2/helper `9to open the menu.")
 LogToConsole("`3[FaRu] `2Loaded Successfully! Type `9/helper `2to open the menu.")
 
 -- =========================================================
+
 
